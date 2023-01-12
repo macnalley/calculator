9062b1f5cf6514adbdbbbd043b8c710b66ec6545
McKenzie <mckenzie.nalley@gmail.com>
2023-01-12 13:20:48 -0500

    Added basic math control from UI.

--- script.js
+++ script.js
@@ -1,20 +1,94 @@
 let displayText = "";
+let memory = "";
+let isAnswerDisplayed = false;
 
 // DOM elements
-const NumBtns = document.querySelectorAll('.number');
 const displayElement = document.querySelector('#display');
+const numBtns = document.querySelectorAll('.number');
+const operBtns = document.querySelectorAll('.operator');
+const equalsBtn = document.querySelector('#equals');
+const clearBtn = document.querySelector('#clear');
 
 // Events
-NumBtns.forEach(btn => btn.addEventListener('click', NumClick));
+numBtns.forEach(btn => btn.addEventListener('click', numClick));
+operBtns.forEach(btn => btn.addEventListener('click', operClick));
+equalsBtn.addEventListener('click', equalsClick);
+clearBtn.addEventListener('click', clear);
 
 
-function NumClick(e) {
-    if (displayText.length <= 15) {
+function numClick(e) {
+    if (isAnswerDisplayed) {
+        displayText = '';
+        displayText += `${e.target.textContent}`;
+    }
+    else if (displayText.length <= 11 & !hasOperatorApplied(displayText)) {
+        displayText += `${e.target.textContent}`;
+    } else if (hasOperatorApplied(displayText)) {
+        memory = displayText;
+        displayText = "";
         displayText += `${e.target.textContent}`;
     }
     updateDisplay();
+    isAnswerDisplayed = false;
 }
 
+function operClick(e) {
+    let operator = e.target.textContent;
+    
+    if (memory.length > 0) {
+        let memoryOperator = memory.slice(-1);
+        let x = parseInt(memory.slice(0, memory.length - 1));
+        let y = parseInt(displayText);
+        displayText = operate(memoryOperator, x, y) + operator;
+    }
+    else if (!hasOperatorApplied(displayText)) {
+        displayText += `${operator}`;
+    } else {
+        displayText = displayText.slice(0, displayText.length - 1) + operator;
+    }
+    updateDisplay();
+    isAnswerDisplayed = false;
+
+}
+
+function equalsClick(e) {
+    if (memory.length > 0 && displayText.length > 0) {
+        let operator = memory.slice(-1);
+        let x = parseInt(memory.slice(0, memory.length - 1));
+        let y = parseInt(displayText);
+        displayText = `${operate(operator, x, y)}`;
+        memory = '';
+        updateDisplay();
+        isAnswerDisplayed = true;
+    }
+}
+
+function clear(e) {
+    displayText = "";
+    memory = "";
+    updateDisplay();
+    isAnswerDisplayed = false;
+}
+
+function hasOperatorApplied(text) {
+    switch (text.slice(-1)) {
+        case '+':
+            return true;
+            break;
+        case '-':
+            return true;
+            break;
+        case '*':
+            return true;
+            break;
+        case '÷': 
+            return true;
+            break;
+        default:
+            return false;
+            break;
+    }
+}
 
 // Display functions
 function updateDisplay() {
@@ -49,7 +123,7 @@ function operate(operator, x, y) {
         case '*':
             return multiply(x,y);
             break;
-        case '/':
+        case '÷':
             return divide(x,y);
             break;
         default:
